c1a334074d23cfedce2088d540adc23219b4761f
Edouard <edou4rd@gmail.com>
2012-09-07 21:31:17 -0400

    vimeo

---
+++ sources/vimeo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Encoding: utf-8
+# -----------------------------------------------------------------------------
+# Project : a Serious Toolkit
+# -----------------------------------------------------------------------------
+# Author : Edouard Richard                                  <edou4rd@gmail.com>
+# -----------------------------------------------------------------------------
+# License : GNU Lesser General Public License
+# -----------------------------------------------------------------------------
+# Creation : 07-Sep-2012
+# Last mod : 07-Sep-2012
+# -----------------------------------------------------------------------------
+import urllib, json
+
+class Vimeo:
+
+	URL = "http://vimeo.com/api/v2/video"
+
+	@staticmethod
+	def getInfo(id):
+		try:
+			filehandle = urllib.urlopen("%s/%s.json" % (Vimeo.URL, id))
+			return json.load(filehandle)[0]
+		except:
+			return None
+
+if __name__ == "__main__":
+	""" test """
+	from pprint import pprint as pp
+	infos = Vimeo.getInfo("6437816")
+	assert json.dumps(infos)
